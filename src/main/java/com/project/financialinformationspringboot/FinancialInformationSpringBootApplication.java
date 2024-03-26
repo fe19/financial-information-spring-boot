86b8bd9d966bb8f021dff8f49e1cb336c7cc06a8
fe19 <flth741@gmail.com>
2024-03-26 11:41:35 +0100

    Initial commit

---
+++ src/main/java/com/project/financialinformationspringboot/FinancialInformationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.financialinformationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInformationSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialInformationSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/financialinformationspringboot/FinancialInformationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.financialinformationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInformationSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

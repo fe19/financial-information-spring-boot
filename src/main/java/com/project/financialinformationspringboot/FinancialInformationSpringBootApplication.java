9286d35b673d6a4f3d9389eba14ea8c9532f62ff
fe19 <flth741@gmail.com>
2024-04-04 10:46:15 +0200

    Add todo

--- src/main/java/com/project/financialinformationspringboot/FinancialInformationSpringBootApplication.java
+++ src/main/java/com/project/financialinformationspringboot/FinancialInformationSpringBootApplication.java
@@ -9,8 +9,6 @@ public class FinancialInformationSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinancialInformationSpringBootApplication.class, args);
         System.out.println("TEST");
-
-        // TODO OAuth
     }
 
 }

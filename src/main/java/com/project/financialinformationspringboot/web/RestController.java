7fc8fb89a3784f2741958770d5edd61119111041
fe19 <flth741@gmail.com>
2024-04-15 20:39:16 +0200

    Extend test with parsing the response

--- src/main/java/com/project/financialinformationspringboot/web/RestController.java
+++ src/main/java/com/project/financialinformationspringboot/web/RestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("https://api.coinbase.com/v2/prices/BTC-USD/buy")
 public class RestController {
-
     @GetMapping("/request")
     private ResponseEntity<String> findById() {
         return ResponseEntity.ok().build();

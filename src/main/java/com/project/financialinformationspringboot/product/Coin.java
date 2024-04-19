6143dc02ec6d4600c12d1186a073c6e31f527ed3
fe19 <flth741@gmail.com>
2024-04-19 18:10:42 +0200

    Add date to record

--- src/main/java/com/project/financialinformationspringboot/product/Coin.java
+++ src/main/java/com/project/financialinformationspringboot/product/Coin.java
@@ -2,5 +2,7 @@ package com.project.financialinformationspringboot.product;
 
 import org.springframework.data.annotation.Id;
 
-public record Coin(@Id Long id, String name, Double amount, String currency) {
+import java.util.Date;
+
+public record Coin(@Id Long id, String name, Double amount, String currency, Date date) {
 }

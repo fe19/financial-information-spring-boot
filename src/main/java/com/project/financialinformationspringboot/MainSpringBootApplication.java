114ca7ccc2751bb6021cdc2334c795f8193825ab
fe19 <flth741@gmail.com>
2024-04-22 19:08:01 +0200

    Activate schema.sql on startup

--- src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
+++ src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
@@ -1,6 +1,8 @@
 package com.project.financialinformationspringboot;
 
 import com.project.financialinformationspringboot.data.Coin;
+import com.project.financialinformationspringboot.data.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -8,6 +10,9 @@ import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MainSpringBootApplication {
+
+    @Autowired
+    private CoinRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(MainSpringBootApplication.class, args);
@@ -26,6 +31,7 @@ public class MainSpringBootApplication {
         btc.setName("BTC");
         btc.setCurrency("USD");
         btc.setAmount(100000.00);
+        this.repository.save(btc);
         System.out.println("Created a new coin: " + btc);
     }
 }

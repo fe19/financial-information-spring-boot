269fb3e0031673829e3b852f0f0ce8ac3ea01f5c
fe19 <flth741@gmail.com>
2024-04-22 18:42:56 +0200

    Create a sample coin

--- src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
+++ src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
@@ -3,6 +3,7 @@ package com.project.financialinformationspringboot;
 import com.project.financialinformationspringboot.data.Coin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
@@ -13,9 +14,18 @@ public class MainSpringBootApplication {
 
     }
 
-    @EventListener(MainSpringBootApplication.class)
+    @EventListener(ApplicationReadyEvent.class)
     public void runAfterStartup() {
         System.out.println("SPRING BOOT APPLICATION STARTED");
+        createCoin();
     }
 
+    private void createCoin() {
+        Coin btc = new Coin();
+        btc.setId(1L);
+        btc.setName("BTC");
+        btc.setCurrency("USD");
+        btc.setAmount(100000.00);
+        System.out.println("Created a new coin: " + btc);
+    }
 }
--- src/main/java/com/project/financialinformationspringboot/data/Coin.java
+++ src/main/java/com/project/financialinformationspringboot/data/Coin.java
@@ -1,14 +1,22 @@
 package com.project.financialinformationspringboot.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table
 public class Coin {
     @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private double amount;
+
+    @Column(name = "CURRENCY", nullable = false)
+    private String currency;
 
     public void setId(Long id) {
         this.id = id;
@@ -16,5 +24,33 @@ public class Coin {
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String toString() {
+        return "ID = " + id + " NAME = " + name + " AMOUNT = " + amount + " CURRENCY = " + currency ;
     }
 }

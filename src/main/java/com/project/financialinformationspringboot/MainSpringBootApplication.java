fdecc78b207575d6ba153f4ba83fc8291c4785cf
fe19 <flth741@gmail.com>
2024-04-22 18:33:59 +0200

    Add method after start upa

--- src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
+++ src/main/java/com/project/financialinformationspringboot/MainSpringBootApplication.java
@@ -1,14 +1,21 @@
 package com.project.financialinformationspringboot;
 
+import com.project.financialinformationspringboot.data.Coin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MainSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainSpringBootApplication.class, args);
-        System.out.println("TEST");
+
+    }
+
+    @EventListener(MainSpringBootApplication.class)
+    public void runAfterStartup() {
+        System.out.println("SPRING BOOT APPLICATION STARTED");
     }
 
 }
